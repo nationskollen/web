975cfaf290aad718d2de6b8f5274cd9b493cee62
ariaassadi <aria.assadi@gmail.com>
2021-04-12 17:12:08 +0200

    Added (almost) all design for the login page

--- pages/login.js
+++ pages/login.js
@@ -3,14 +3,31 @@ import styles from '../styles/Login.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <div className={styles.container}>
-        <div className={styles.background}></div>
+    <div className={styles.container}>
+      {/* <img src="../img/Uppsala vector.svg" class={styles.background}></img> */}
+      <div className={styles.background}></div>
 
-        <div className={styles.header}>
-          <p className={styles.headerText}>NATIONSKOLLEN</p>
-        </div>
+      <div className={styles.header}>
+        <p className={styles.headerText}>NATIONSKOLLEN</p>
       </div>
+
+      <table className={styles.loginContainer}>
+        <tr>
+          <div className={styles.text}>EMAIL</div>
+          <div className={styles.textPanel}></div>
+        </tr>
+        <tr>
+          <div className={styles.text}>PASSWORD</div>
+          <div className={styles.textPanel}>
+            <div className={styles.text}>SHOW</div>
+          </div>
+        </tr>
+        <tr>
+          <div className={styles.loginPanel}>
+            <div className={styles.loginButtonText}>LOGIN</div>
+          </div>
+        </tr>
+      </table>
     </div>
   )
 }
