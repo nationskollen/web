37e462767bc6af4eb914d518269ba07a646732ea
ariaassadi <aria.assadi@gmail.com>
2021-04-13 11:49:13 +0200

    Added input textarea for email and password. Added functionality for the show-button

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,19 @@
 import Head from 'next/head'
+import React, { useState } from "react";
 import styles from '../styles/Login.module.css'
 
 export default function Home() {
+  const [type, setType] = useState("password");
+  const togglePasswordVisibility = () => {
+    console.log("Toggled password visibility");
+    if (type == "password") {
+      setType("text");
+    }
+    else {
+      setType("password");
+    }
+  };
+
   return (
     <div className={styles.container}>
       {/* <img src="../img/Uppsala vector.svg" class={styles.background}></img> */}
@@ -14,12 +26,15 @@ export default function Home() {
       <table className={styles.loginContainer}>
         <tr>
           <div className={styles.text}>EMAIL</div>
-          <div className={styles.textPanel}></div>
+          <div className={styles.textPanel}>
+            <input id={styles.inputEmail}></input>
+          </div>
         </tr>
         <tr>
           <div className={styles.text}>PASSWORD</div>
           <div className={styles.textPanel}>
-            <div className={styles.text}>SHOW</div>
+            <input id={styles.inputPassword} rows="1" type={type}></input>
+            <button className={styles.showButton} onClick={togglePasswordVisibility}>SHOW</button>
           </div>
         </tr>
         <tr>
@@ -30,4 +45,15 @@ export default function Home() {
       </table>
     </div>
   )
+}
+
+function loginButton() {
+  var email     = document.getElementById(style.inputEmail);
+  var password  = document.getElementById(style.inputPassword);
+  if(validCredentials()) {
+    true;
+  }
+  else {
+    true;
+  }
 }
