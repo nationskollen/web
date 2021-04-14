bbd83d46a0af2dfc410dce94ed0cd05049657799
Aria Assadi <48809724+ariaassadi@users.noreply.github.com>
2021-04-14 19:06:16 +0200

    Improved style for navbar and header (#12)
    * Improved style for Navbar
    
    * Improved style for Navbar
    
    * Improved style for Header
    
    * Added desriptions for each page in Header

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,50 @@
-import Navbar from '../components/Navbar.js'
+import React from 'react';
 import styles from '../styles/Header.module.css'
+import Router from 'next/router'
 import * as Icons from 'react-icons/hi'
 
 
 const Header = () => {
+    const logOut = () => {
+        console.log("User logged out");
+        //Load login page
+        Router.push("/login");
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.leftHeader}>
 
                 {/* //TODO Active title from navbar */}
 
-                <p className={styles.ptext}>BÖR STÅ VILKEN FLIK man är på</p>
+                <p className={styles.ptext}>{getDescription(window.location.pathname)}</p>
             </div>
 
             <div className={styles.leftHeader}>
+                {/* //TODO Replace this icon with the nation logo */}
                 <Icons.HiAcademicCap className={styles.icon}/>
                 <p>Admin</p>
-                <Icons.HiChevronDown className={styles.icon}/>
+                <div className={styles.line}></div>
+                <button className={styles.logOutButton} onClick={logOut}>
+                    Logga ut
+                </button>
             </div>
         </div>
     )
 }
 
 export default Header
+
+function getDescription(page) {
+    return descriptions[page];
+}
+
+var descriptions = {
+    //TODO Add better descriptions
+    "/":                "Välkommen till Nationskollens hemsida för admins",
+    "/news":            "Hantera notifikationer och meddelanden till prenumeranter",
+    "/entrance":        "Ändra antalet besökare på nationen i realtid",
+    "/businessHours":   "Redigera nationens öppettider",
+    "/nation":          "Redigera nationens information och beskrivning",
+    "/foodMenu":        "Redigera restaurangens meny"
+}
