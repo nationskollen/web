3595f18b1955592cb4804e798d689c1f2f9999d0
ariaassadi <aria.assadi@gmail.com>
2021-04-14 16:16:20 +0200

    Improved style for Header

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,16 @@
 import Navbar from '../components/Navbar.js'
 import styles from '../styles/Header.module.css'
+import Router from 'next/router'
 import * as Icons from 'react-icons/hi'
 
 
 const Header = () => {
+    const logOut = () => {
+        console.log("User logged out");
+        //Load login page
+        Router.push("/login");
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.leftHeader}>
@@ -14,9 +21,13 @@ const Header = () => {
             </div>
 
             <div className={styles.leftHeader}>
+                {/* //TODO Replace this icon with nation logo */}
                 <Icons.HiAcademicCap className={styles.icon}/>
                 <p>Admin</p>
-                <Icons.HiChevronDown className={styles.icon}/>
+                <div className={styles.line}></div>
+                <button className={styles.logOutButton} onClick={logOut}>
+                    Logga ut
+                </button>
             </div>
         </div>
     )
