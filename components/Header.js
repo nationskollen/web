35b6c5215b5da52e2feab1d162681061c4ecb591
ariaassadi <aria.assadi@gmail.com>
2021-04-14 18:22:03 +0200

    Added desriptions for each page in Header

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import Navbar from '../components/Navbar.js'
+import React from 'react';
 import styles from '../styles/Header.module.css'
 import Router from 'next/router'
 import * as Icons from 'react-icons/hi'
@@ -17,11 +17,11 @@ const Header = () => {
 
                 {/* //TODO Active title from navbar */}
 
-                <p className={styles.ptext}>BÖR STÅ VILKEN FLIK man är på</p>
+                <p className={styles.ptext}>{getDescription(window.location.pathname)}</p>
             </div>
 
             <div className={styles.leftHeader}>
-                {/* //TODO Replace this icon with nation logo */}
+                {/* //TODO Replace this icon with the nation logo */}
                 <Icons.HiAcademicCap className={styles.icon}/>
                 <p>Admin</p>
                 <div className={styles.line}></div>
@@ -34,3 +34,17 @@ const Header = () => {
 }
 
 export default Header
+
+function getDescription(page) {
+    return descriptions[page];
+}
+
+var descriptions = {
+    //TODO Add better descriptions
+    "/":                "Välkommen till Nationskollens hemsida för admins",
+    "/news":            "Hantera notifikationer och meddelanden till prenumeranter",
+    "/entrance":        "Ändra antalet besökare på nationen i realtid",
+    "/businessHours":   "Redigera nationens öppettider",
+    "/nation":          "Redigera nationens information och beskrivning",
+    "/foodMenu":        "Redigera restaurangens meny"
+}
