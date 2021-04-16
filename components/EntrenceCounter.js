1aca131ee5057ba4067d381c2cad81593791722d
ariaassadi <aria.assadi@gmail.com>
2021-04-16 16:38:24 +0200

    Added empty component EntrenceCounter

---
+++ components/EntrenceCounter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/EntrenceCounter.module.css'
+
+const EntrenceCounter = () => {
+    return (
+        <div className={styles.container}>
+            <p>Hej</p>
+        </div>
+    );
+};
+
+export default EntrenceCounter;
