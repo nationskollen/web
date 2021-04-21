b84376cb028a2ebff4f3f30b2d87eb53ef82e029
wallskog <carl.wallskog@gmail.com>
2021-04-21 09:32:08 +0200

    added a row, need to format this

--- components/Datatable.js
+++ components/Datatable.js
@@ -4,12 +4,14 @@ import styles from "../styles/Datatable.module.css";
 
 export default function Datatable({ data }) {
   const columns = data[0] && Object.keys(data[0]);
+  const info = "hej";
   return (
     <table cellPadding={0} cellSpacing={0} className={styles.table}>
       <thead className={styles.thead}>
         <tr>
           {data[0] &&
             columns.map((heading) => <th className={styles.th}>{heading}</th>)}
+          <th className={styles.th}>Hej</th>
         </tr>
       </thead>
       <tbody className={styles.tbody}>
@@ -18,6 +20,7 @@ export default function Datatable({ data }) {
             {columns.map((columns) => (
               <td className={styles.td}>{row[columns]}</td>
             ))}
+            <td className={styles.td}>{info}</td>
           </tr>
         ))}
       </tbody>
--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,18 @@
-import '../styles/globals.css'
-import { Client } from '@dsp-krabby/sdk'
-import { Provider } from '@dsp-krabby/sdk/lib/react'
+import "../styles/globals.css";
+import { Client } from "@dsp-krabby/sdk";
+import { Provider } from "@dsp-krabby/sdk/lib/react";
 
 const client = Client({
-    development: process.env.NODE_ENV === 'development',
-    useWebSockets: false
-})
+  development: process.env.NODE_ENV === "development",
+  useWebSockets: false,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider client={client}>
       <Component {...pageProps} />
     </Provider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
