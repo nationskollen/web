43710e6c02896568648786b6b5710cfb8c0b671c
wallskog <carl.wallskog@gmail.com>
2021-04-21 20:55:40 +0200

    changed a little css for Cards & small changes to Events

--- components/Datatable.js
+++ components/Datatable.js
@@ -1,4 +1,5 @@
 import React from "react";
+import * as Icons from "react-icons/hi";
 
 import styles from "../styles/Datatable.module.css";
 
@@ -9,18 +10,26 @@ export default function Datatable({ data }) {
     <table cellPadding={0} cellSpacing={0} className={styles.table}>
       <thead className={styles.thead}>
         <tr>
+          <th className={styles.th}>select</th>
           {data[0] &&
             columns.map((heading) => <th className={styles.th}>{heading}</th>)}
-          <th className={styles.th}>Hej</th>
+          <th className={styles.th}>Actions</th>
         </tr>
       </thead>
       <tbody className={styles.tbody}>
         {data.map((row) => (
           <tr>
+            <td className={styles.td}>
+              <input type="checkbox" />
+            </td>
             {columns.map((columns) => (
               <td className={styles.td}>{row[columns]}</td>
             ))}
-            <td className={styles.td}>{info}</td>
+            <td className={styles.td}>
+              <Icons.HiEye />
+              <Icons.HiPencil />
+              <Icons.HiTrash />
+            </td>
           </tr>
         ))}
       </tbody>
