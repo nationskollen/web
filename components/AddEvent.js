f617417a1de9f5be7cee219887284ccdb0f7f16b
wallskog <carl.wallskog@gmail.com>
2021-04-22 10:43:31 +0200

    changed css for AddEvent

--- components/AddEvent.js
+++ components/AddEvent.js
@@ -3,51 +3,50 @@ import styles from "../styles/AddEvent.module.css";
 
 const AddEvent = (props) => {
   return (
-    //   TODO Fixa hela jälva formen
+    //TODO Fixa hela jälva formen
     <div className={styles.container}>
       <form className={styles.form}>
         <div className={styles.header}></div>
-        <div className={styles.start}>
-          {/* Start: */}
-          <lable className={styles.lable}>Start Time</lable>
-          <input type="datetime-local" className={styles.date} />
-        </div>
-        <div className={styles.start}>
-          {/* End: */}
-          <lable className={styles.lable}>End Time</lable>
-          <input type="datetime-local" className={styles.date} />
-        </div>
-        <div className={styles.start}>
-          {/* Title: */}
-          <lable className={styles.lable}>Title </lable>
-          <input type="text" className={styles.input} />
-        </div>
-        <div className={styles.start}>
-          {/* Description: */}
-          <lable className={styles.lable}>Description</lable>
+        {/* Start: */}
+        <label className={styles.label}>
+          Start Time
+          <input className={styles.input} type="datetime-local" />
+        </label>
+        {/* End: */}
+        <label className={styles.label}>
+          End Time
+          <input className={styles.input} type="datetime-local" />
+        </label>
+        {/* Title: */}
+        <label className={styles.label}>
+          Title
+          <input className={styles.input} type="text" />
+        </label>
+        {/* Description: */}
+        <label className={styles.label}>
+          Description
           <textarea
-            id="w3review"
             name="w3review"
             rows="15"
             cols="60"
             className={styles.textarea}
             placeholder={"A quick description of the event"}
           ></textarea>
-        </div>
-        <div className={styles.start}>
-          {/* Tags: */}
-          <lable className={styles.lable}>Tags</lable>
-          <input type="text" className={styles.input} />
-        </div>
-        <div className={styles.start}>
-          {/* Locations: */}
-          <lable className={styles.lable}>Locations</lable>
-          <input type="search" className={styles.input} />
-        </div>
-        <div className={styles.start}>
-          {/* Locations: */}
-          <lable className={styles.lable}>Type of event</lable>
-          <select id="cars" name="cars" className={styles.input}>
+        </label>
+        {/* Tags: */}
+        <label className={styles.label}>
+          Tags
+          <input className={styles.input} type="text" />
+        </label>
+        {/* Locations: */}
+        <label className={styles.label}>
+          Locations
+          <input className={styles.input} type="search" />
+        </label>
+        {/* Type of event: */}
+        <label className={styles.label}>
+          Type of event
+          <select className={styles.input} id="cars" name="cars">
             <option value="Frukost">Frukost</option>
             <option value="Brunch">Brunch</option>
             <option value="Fika">Fika</option>
@@ -61,28 +60,36 @@ const AddEvent = (props) => {
             <option value="Konsert">Konsert</option>
             <option value="Övrigt">Övrigt</option>
           </select>
-        </div>
-        <div className={styles.start}>
-          {/* Require Nationskort? */}
-          <lable className={styles.lable}>Only students</lable>
-          <input type="checkbox" />
-        </div>
-        <div className={styles.start}>
-          {/* Require Nationskort? */}
-          <lable className={styles.lable}>Only members</lable>
-          <input type="checkbox" />
-        </div>
-        <div className={styles.start}>
-          {/* Image header: */}
-          <lable className={styles.lable}>Image header</lable>
-          <input type="file" id="myfile" name="myfile" />
-        </div>
+        </label>
+        {/* Students only */}
+        <label className={styles.label}>
+          Only Students
+          <input className={styles.input} type="checkbox" />
+        </label>
+        {/* Members only*/}
+        <label className={styles.label}>
+          Only members
+          <input className={styles.input} type="checkbox" />
+        </label>
+        {/* Image header: */}
+        <label className={styles.label}>
+          Image header
+          <input
+            className={styles.input}
+            type="file"
+            id="myfile"
+            name="myfile"
+          />
+        </label>
 
-        <input
-          type="submit"
-          value="Create Event"
-          className={styles.input}
-        ></input>
+        <label className={styles.label}>
+          <input
+            className={styles.input}
+            className={styles.create}
+            type="submit"
+            value="Create Event"
+          ></input>
+        </label>
       </form>
     </div>
   );
