19fdfd27c7422e1305f89ee49817302ccdadb09c
wallskog <carl.wallskog@gmail.com>
2021-04-27 09:36:17 +0200

    fullösning

--- pages/index.js
+++ pages/index.js
@@ -18,129 +18,125 @@ export default function Home() {
   const [value, onChange] = useState(new Date());
 
   return (
-    <div>
-      <div className={styles.container}>
-        <Navbar />
-
-        <div className={styles.header}>
-          <Header />
-          <div className={container.container}>
-            <div className={container.cards}>
-              <Card>
-                <Chart
-                  header={"Besökare"}
-                  width={400}
-                  height={400}
-                  data={{
-                    labels: ["Medlemmar", "Ickemedlemmar"],
-                    datasets: [
-                      {
-                        data: [300, 50],
-                        backgroundColor: ["#FF6384", "#36A2EB"],
-                        hoverBackgroundColor: ["#FF6384", "#36A2EB"],
-                      },
-                    ],
-                  }}
-                />
-              </Card>
-              <Card>
-                <Chart
-                  header={"Försäljning"}
-                  width={400}
-                  height={400}
-                  data={{
-                    labels: ["Öl", "Öl2", "Öl3", "Öl4", "Cider"],
-                    datasets: [
-                      {
-                        data: [300, 50, 10, 321, 12],
-                        backgroundColor: [
-                          "#FF6384",
-                          "#F53384",
-                          "#F14384",
-                          "#FF3284",
-                          "#36A2EB",
-                        ],
-                        hoverBackgroundColor: [
-                          "#FF6384",
-                          "#FF6384",
-                          "#FF6384",
-                          "#FF6384",
-                          "#36A2EB",
-                        ],
-                      },
-                    ],
-                  }}
-                />
-              </Card>
-              <Card>
-                <Chart
-                  header={"Försäljning"}
-                  width={400}
-                  height={400}
-                  data={{
-                    labels: ["Öl", "Öl2", "Öl3", "Öl4", "Cider"],
-                    datasets: [
-                      {
-                        data: [300, 50, 10, 321, 12],
-                        backgroundColor: [
-                          "#FF6384",
-                          "#F53384",
-                          "#F14384",
-                          "#FF3284",
-                          "#36A2EB",
-                        ],
-                        hoverBackgroundColor: [
-                          "#FF6384",
-                          "#FF6384",
-                          "#FF6384",
-                          "#FF6384",
-                          "#36A2EB",
-                        ],
-                      },
-                    ],
-                  }}
-                />
-              </Card>
-              <Card>
-                <Chart
-                  header={"Försäljning"}
-                  width={400}
-                  height={400}
-                  data={{
-                    labels: ["Öl", "Öl2", "Öl3", "Öl4", "Cider"],
-                    datasets: [
-                      {
-                        data: [300, 50, 10, 321, 12],
-                        backgroundColor: [
-                          "#FF6384",
-                          "#F53384",
-                          "#F14384",
-                          "#FF3284",
-                          "#36A2EB",
-                        ],
-                        hoverBackgroundColor: [
-                          "#FF6384",
-                          "#FF6384",
-                          "#FF6384",
-                          "#FF6384",
-                          "#36A2EB",
-                        ],
-                      },
-                    ],
-                  }}
-                />
-              </Card>
-            </div>
-
-            <div className={container.main}>
-              <Main />
-
-              <Rightbar>
-                <Calendar />
-              </Rightbar>
-            </div>
+    <div className={styles.container}>
+      <Navbar />
+      <Header />
+      <div className={container.container}>
+        <main>
+          <div className={container.cards}>
+            <Card>
+              <Chart
+                header={"Besökare"}
+                width={400}
+                height={400}
+                data={{
+                  labels: ["Medlemmar", "Ickemedlemmar"],
+                  datasets: [
+                    {
+                      data: [300, 50],
+                      backgroundColor: ["#FF6384", "#36A2EB"],
+                      hoverBackgroundColor: ["#FF6384", "#36A2EB"],
+                    },
+                  ],
+                }}
+              />
+            </Card>
+            <Card>
+              <Chart
+                header={"Försäljning"}
+                width={400}
+                height={400}
+                data={{
+                  labels: ["Öl", "Öl2", "Öl3", "Öl4", "Cider"],
+                  datasets: [
+                    {
+                      data: [300, 50, 10, 321, 12],
+                      backgroundColor: [
+                        "#FF6384",
+                        "#F53384",
+                        "#F14384",
+                        "#FF3284",
+                        "#36A2EB",
+                      ],
+                      hoverBackgroundColor: [
+                        "#FF6384",
+                        "#FF6384",
+                        "#FF6384",
+                        "#FF6384",
+                        "#36A2EB",
+                      ],
+                    },
+                  ],
+                }}
+              />
+            </Card>
+            <Card>
+              <Chart
+                header={"Försäljning"}
+                width={400}
+                height={400}
+                data={{
+                  labels: ["Öl", "Öl2", "Öl3", "Öl4", "Cider"],
+                  datasets: [
+                    {
+                      data: [300, 50, 10, 321, 12],
+                      backgroundColor: [
+                        "#FF6384",
+                        "#F53384",
+                        "#F14384",
+                        "#FF3284",
+                        "#36A2EB",
+                      ],
+                      hoverBackgroundColor: [
+                        "#FF6384",
+                        "#FF6384",
+                        "#FF6384",
+                        "#FF6384",
+                        "#36A2EB",
+                      ],
+                    },
+                  ],
+                }}
+              />
+            </Card>
+            <Card>
+              <Chart
+                header={"Försäljning"}
+                width={400}
+                height={400}
+                data={{
+                  labels: ["Öl", "Öl2", "Öl3", "Öl4", "Cider"],
+                  datasets: [
+                    {
+                      data: [300, 50, 10, 321, 12],
+                      backgroundColor: [
+                        "#FF6384",
+                        "#F53384",
+                        "#F14384",
+                        "#FF3284",
+                        "#36A2EB",
+                      ],
+                      hoverBackgroundColor: [
+                        "#FF6384",
+                        "#FF6384",
+                        "#FF6384",
+                        "#FF6384",
+                        "#36A2EB",
+                      ],
+                    },
+                  ],
+                }}
+              />
+            </Card>
           </div>
-        </div>
+
+          <div className={container.main}>
+            <Main />
+            <Rightbar>
+              <Calendar />
+            </Rightbar>
+          </div>
+        </main>
       </div>
     </div>
   );
