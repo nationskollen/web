10e854a61a1d3cc08d5477725836eda40def0167
ariaassadi <aria.assadi@gmail.com>
2021-04-28 09:30:44 +0200

    Started implementing to sdk

--- components/DayHours.js
+++ components/DayHours.js
@@ -1,19 +1,52 @@
 import React from 'react';
 import { useState, useEffect } from "react";
 import styles from '../styles/DayHours.module.css';
-import { useLogin } from '@dsp-krabby/sdk'
+import { useAsyncCallback } from 'react-async-hook'
+import { useApi } from '@dsp-krabby/sdk'
 
 //Creates a row with business hours for a choosen day
 const ProgressBar = (props) => {
     const { day, saveChanges } = props;
+    const api = useApi();
+    const [openingHourData, setOpeningHourData] = useState(null);
+    const response = useAsyncCallback(() => api.openingHours.update(locationId, openingHourId, openingHourData));
+
     //TODO Set deafult value from sdk
     const [isOpen, setIsOpen] = useState(false);
     const [start, setStart] = useState("08:00");
     const [end, setEnd] = useState("20:00");
 
+
+    const dayToNumber = day => {
+        switch (day) {
+            case "Monday":
+                return 0
+            case "Tuesday":
+                return 1
+            case "Wednesday":
+                return 2
+            case "Thursday":
+                return 3
+            case "Friday":
+                return 4
+            case "Saturday":
+                return 5
+            case Sunday:
+                return 6
+            default:
+                return 7
+        }
+    }
     useEffect(() => {
         //TODO Change values in sdk
-        console.log("performRefresh");
+        setOpeningHourData({
+            ...openingHourData,
+            open:       start,
+            close:      end,
+            is_open:    isOpen
+        })
+        response.execute();
+        console.log("Saved opening hours for " + day);
       }, [saveChanges])
 
     return (
