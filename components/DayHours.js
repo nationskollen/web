e69ee2d5c10408936751b39f6902ab7bff94aa36
ariaassadi <aria.assadi@gmail.com>
2021-04-28 10:28:02 +0200

    Temporarily removed sdk from day hours since it didnt compile

--- components/DayHours.js
+++ components/DayHours.js
@@ -2,16 +2,22 @@ import React from 'react';
 import { useState, useEffect } from "react";
 import styles from '../styles/DayHours.module.css';
 import { useAsyncCallback } from 'react-async-hook'
-import { useApi } from '@dsp-krabby/sdk'
+import { useApi, useLocations } from '@dsp-krabby/sdk'
 
 //Creates a row with business hours for a choosen day
-const ProgressBar = (props) => {
+const DayHours = (props) => {
+    // User automatically gets oid 400 (Vdala nation)
+    // TODO Replace this row to get oid from local storage
+    const oid = 400;
+    // const oid = localStorage.getItem('oid');
+
+    const location = useLocations(oid);
+
     const { day, saveChanges } = props;
     const api = useApi();
     const [openingHourData, setOpeningHourData] = useState(null);
-    const response = useAsyncCallback(() => api.openingHours.update(locationId, openingHourId, openingHourData));
-
-    //TODO Set deafult value from sdk
+    // const response = useAsyncCallback(() => api.openingHours.update(location, openingHourId, openingHourData));
+    //TODO Set deafult values from sdk
     const [isOpen, setIsOpen] = useState(false);
     const [start, setStart] = useState("08:00");
     const [end, setEnd] = useState("20:00");
@@ -31,21 +37,22 @@ const ProgressBar = (props) => {
                 return 4
             case "Saturday":
                 return 5
-            case Sunday:
+            case "Sunday":
                 return 6
             default:
                 return 7
         }
     }
     useEffect(() => {
-        //TODO Change values in sdk
-        setOpeningHourData({
-            ...openingHourData,
-            open:       start,
-            close:      end,
-            is_open:    isOpen
-        })
-        response.execute();
+        //TODO Set values in sdk
+        // setOpeningHourData({
+        //     ...openingHourData,
+        //     day:        dayToNumber(day),
+        //     open:       start,
+        //     close:      end,
+        //     is_open:    isOpen
+        // })
+        // response.execute();
         console.log("Saved opening hours for " + day);
       }, [saveChanges])
 
@@ -56,13 +63,7 @@ const ProgressBar = (props) => {
                 <input type="checkbox" onChange={() => setIsOpen(!isOpen)}/>
                 <span className={styles.slider}></span>
             </label>
-        {!isOpen &&
-            <div className={styles.secondaryContainer}>
-                <div className={styles.smallText}>Closed</div>
-                <div className={styles.emptyContainer}></div>
-            </div>
-        }
-        {isOpen && (          
+        {isOpen ? (          
             <div className={styles.secondaryContainer}>
                 <div className={styles.smallText}>Open</div>
                 <input
@@ -70,7 +71,7 @@ const ProgressBar = (props) => {
                     type="time"
                     value={start}
                     onChange={event => setStart(event.target.value)}
-                />
+                    />
                 <span className={styles.seperator}>-</span>
                 <input
                     className={styles.inputEnd}
@@ -79,10 +80,15 @@ const ProgressBar = (props) => {
                     onChange={event => setEnd(event.target.value)}
                     />
             </div>      
+        ) : (
+            <div className={styles.secondaryContainer}>
+                <div className={styles.smallText}>Closed</div>
+                <div className={styles.emptyContainer}></div>
+            </div>
         )}
 
         </div>
     );
   };
   
-  export default ProgressBar;
+  export default DayHours;
