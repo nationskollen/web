f397f3cf5b973766f51291c13adefc644b4f284d
Aria Assadi <48809724+ariaassadi@users.noreply.github.com>
2021-04-28 14:19:31 +0200

    Added design for opening hours (#26)
    * User automatically gets oid 400 (Vdala)
    
    * Added day hours component
    
    * Added toggle switch
    
    * Improved css of day hours
    
    * Position of day hours component is no longer absolute
    
    * Added layout for business hours page
    
    * Added type time to opening hours input panels
    
    * Add save changes button
    
    * Added TODOs
    
    * Business hours pages has the same layout at the rest
    
    * Started implementing to sdk
    
    * Temporarily removed sdk from day hours since it didnt compile
    
    * Added deviating openings hours
    
    * Renamed DayHours component to OpeningHoursDay
    
    * Removed calender from business hours page
    
    * Renamed DayHours component to OpeningHoursDay
    
    * Added new component OpeningHoursDate
    
    * Renamed styles/OpeningHoursDay to styles/OpeningHours with the intension to use for two components
    
    * Seperated date and day component
    
    * Removed borders
    
    * Changed width
    
    * Removed empty container

--- pages/businessHours.js
+++ pages/businessHours.js
@@ -9,32 +9,50 @@ import Chart from "../components/Chart";
 import Main from "../components/Main";
 import Header from "../components/Header";
 import Rightbar from "../components/Rightbar";
-import DayHours from "../components/DayHours"; 
+import OpeningHoursDay from "../components/OpeningHoursDay"; 
+import OpeningHoursDate from "../components/OpeningHoursDate"; 
 
 import styles from "../styles/BusinessHours.module.css";
 import container from "../styles/Container.module.css";
 import "react-calendar/dist/Calendar.css";
 
 export default function BusinessHours() {
+  const [save, doSave] = useState('0');
+
   return (
     <div>
       <div className={styles.container}>
         <Navbar />
-
-        <div className={styles.header}>
-          <Header />
-          <div className={container.container}>
+        <Header />
+        <div className={container.container}>
+          <main>
             <div className={container.cards}></div>
 
             <div className={container.main}>
               <Main>
-                <DayHours day="Wednesday"></DayHours>
+               <div className={styles.standard}>
+                  <div className={styles.headerText}>Öppettider standard</div>
+                  <OpeningHoursDay day="Monday"    saveChanges={save}></OpeningHoursDay>
+                  <OpeningHoursDay day="Tuesday"   saveChanges={save}></OpeningHoursDay>
+                  <OpeningHoursDay day="Wednesday" saveChanges={save}></OpeningHoursDay>
+                  <OpeningHoursDay day="Thurday"   saveChanges={save}></OpeningHoursDay>
+                  <OpeningHoursDay day="Friday"    saveChanges={save}></OpeningHoursDay>
+                  <OpeningHoursDay day="Saturday"  saveChanges={save}></OpeningHoursDay>
+                  <OpeningHoursDay day="Sunday"    saveChanges={save}></OpeningHoursDay>
+                  <button className={styles.button} onClick={() => doSave(save + 1)}>
+                    <div>Spara ändringar</div>
+                  </button>
+                </div>
+                <div className={styles.addDeviating}>
+                  <div className={styles.headerText}>Ny avvikande öppettid</div>
+                  <OpeningHoursDate saveChanges={save}></OpeningHoursDate>
+                  <button className={styles.button} onClick={() => doSave(save + 1)}>
+                    Lägg till
+                  </button>
+                </div>
               </Main>
-              <Rightbar>
-                <Calendar />
-              </Rightbar>
             </div>
-          </div>
+          </main>
         </div>
       </div>
     </div>
