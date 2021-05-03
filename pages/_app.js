ed9b2b8668dad94d1c5ca47dee2ef1953f2b4914
wallskog <carl.wallskog@gmail.com>
2021-05-03 15:01:33 +0200

    npm hatar mig

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,22 @@
 import "../styles/globals.css";
-import { Client } from "@dsp-krabby/sdk";
-import { Provider } from "@dsp-krabby/sdk/lib/react";
+// import { Client } from "@dsp-krabby/sdk";
+import { Provider } from "@dsp-krabby/sdk";
 
-const client = Client({
-  development: process.env.NODE_ENV === "development",
-  useWebSockets: false,
-});
+// const client = Client({
+//
+//   useWebSockets: false,
+// });
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider client={client}>
+    <Provider
+      config={{
+        development: process.env.NODE_ENV === "development",
+        useWebSockets: false,
+        customHostName: "nationskollen-staging.engstrand.nu",
+        useHTTPS: true,
+      }}
+    >
       <Component {...pageProps} />
     </Provider>
   );
