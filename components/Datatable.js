dce3576cd6f5de75055df08a5ca421e4cdbb5223
wallskog <carl.wallskog@gmail.com>
2021-05-04 11:29:49 +0200

    working filter

--- components/Datatable.js
+++ components/Datatable.js
@@ -4,15 +4,38 @@ import * as Icons from "react-icons/hi";
 import styles from "../styles/Datatable.module.css";
 
 export default function Datatable({ data }) {
+  console.log(data[0]);
+
+  let fruits = ["id", "nation_id", "cover_img_src"];
+  let words = [
+    "mango",
+    "desk",
+    "phone",
+    "banana",
+    "airpods",
+    6,
+    "mouse",
+    "strawberry",
+    9,
+    "raspberry",
+    10,
+  ];
+
   const columns = data[0] && Object.keys(data[0]);
-  // var result = columns.filter((item) => !item.includes("id"));
-  // console.log(result);
+
+  columns.forEach((word, index) => {
+    if (fruits.indexOf(word) !== -1) {
+      columns.pop()[index];
+    }
+  });
+  console.log(columns);
 
   return (
     <table cellPadding={0} cellSpacing={0} className={styles.table}>
       <thead className={styles.thead}>
         <tr>
           <th className={styles.th}>select</th>
+
           {data[0] &&
             columns.map((heading) => <th className={styles.th}>{heading}</th>)}
           <th className={styles.th}>Actions</th>
@@ -29,11 +52,13 @@ export default function Datatable({ data }) {
             ))}
             <td className={styles.td}>
               <div className={styles.icons}>
-                <Icons.HiEye />
-                <a href="/addevents">
+                <a className={styles.icon}>
+                  <Icons.HiEye />
+                </a>
+                <a className={styles.icon} href="/addevents">
                   <Icons.HiPencil />
                 </a>
-                <a href="">
+                <a className={styles.icon} href="">
                   <Icons.HiTrash />
                 </a>
               </div>
