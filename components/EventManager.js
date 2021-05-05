0978b28635ebc937a6d4a1504d886a737f5a1274
wallskog <carl.wallskog@gmail.com>
2021-05-05 21:17:21 +0200

    Fixed uppercase bug in event-search

--- components/EventManager.js
+++ components/EventManager.js
@@ -16,23 +16,19 @@ const EventManager = () => {
     }, [])
 
     function search(rows) {
-        return rows.filter(
-            (row) =>
-                searchColumns.some(
-                    (column) => row[column].toString().toLowerCase().indexOf(query) > -1
-                )
-            // Gammal filtrering
-            // row.name.toString().toLowerCase().indexOf(query) > -1 ||
-            // row.id.toString().toLowerCase().indexOf(query) > -1 ||
-            // row.location_id.toString().toLowerCase().indexOf(query) > -1 ||
-            // row.description.toString().toLowerCase().indexOf(query.toLowerCase()) > -1
+        return rows.filter((row) =>
+            searchColumns.some((column) => row[column].toString().toLowerCase().indexOf(query) > -1)
         )
     }
     const columns = data[0] && Object.keys(data[0])
     return (
         <div>
             <div>
-                <input type="text" value={query} onChange={(event) => setQ(event.target.value)} />
+                <input
+                    type="text"
+                    value={query}
+                    onChange={(event) => setQ(event.target.value.toLowerCase())}
+                />
                 {columns &&
                     columns.map((column) => (
                         <lable>
