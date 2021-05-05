b8a3e241b4ed0a1a3684813145e063d41cc12948
wallskog <carl.wallskog@gmail.com>
2021-05-05 22:15:06 +0200

    Changes to filter on Events

--- components/Datatable.js
+++ components/Datatable.js
@@ -1,59 +1,55 @@
-import React from "react";
-import * as Icons from "react-icons/hi";
+import React from 'react'
+import * as Icons from 'react-icons/hi'
 
-import styles from "../styles/Datatable.module.css";
+import styles from '../styles/Datatable.module.css'
 
 export default function Datatable({ data }) {
-  const filter = ["id", "nation_id", "cover_img_src"];
-  let columns = data[0] && Object.keys(data[0]);
-  console.log(typeof columns);
-  console.log(columns);
+    const filter = ['id', 'nation_id', 'cover_img_src']
+    let columns = data[0] && Object.keys(data[0])
+    console.log(typeof columns)
+    console.log(columns)
 
-  if (typeof columns !== "undefined") {
-    columns.forEach((word, index) => {
-      if (filter.indexOf(word) !== -1) {
-        columns.splice(index, 2);
-      }
-    });
-  }
+    if (typeof columns !== 'undefined') {
+        columns.forEach((word, index) => {
+            if (filter.indexOf(word) !== -1) {
+                columns.splice(index, 2)
+            }
+        })
+    }
 
-  console.log(columns);
+    console.log(columns)
 
-  return (
-    <table cellPadding={0} cellSpacing={0} className={styles.table}>
-      <thead className={styles.thead}>
-        <tr>
-          <th className={styles.th}>select</th>
-          {data[0] &&
-            columns.map((heading) => <th className={styles.th}>{heading}</th>)}
-          <th className={styles.th}>Actions</th>
-        </tr>
-      </thead>
-      <tbody className={styles.tbody}>
-        {data.map((row) => (
-          <tr>
-            <td className={styles.td}>
-              <input type="checkbox" />
-            </td>
-            {columns.map((columns) => (
-              <td className={styles.td}>{row[columns]}</td>
-            ))}
-            <td className={styles.td}>
-              <div className={styles.icons}>
-                <a className={styles.icon}>
-                  <Icons.HiEye />
-                </a>
-                <a className={styles.icon} href="/addevents">
-                  <Icons.HiPencil />
-                </a>
-                <a className={styles.icon} href="">
-                  <Icons.HiTrash />
-                </a>
-              </div>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+    return (
+        <table cellPadding={0} cellSpacing={0} className={styles.table}>
+            <thead className={styles.thead}>
+                <tr className={styles.tr}>
+                    <th className={styles.th}>select</th>
+                    {data[0] && columns.map((heading) => <th className={styles.th}>{heading}</th>)}
+                    <th className={styles.th}>Actions</th>
+                </tr>
+            </thead>
+            <tbody className={styles.tbody}>
+                {data.map((row) => (
+                    <tr>
+                        <td className={styles.td}>
+                            <input type="checkbox" />
+                        </td>
+                        {columns.map((columns) => (
+                            <td className={styles.td}>{row[columns]}</td>
+                        ))}
+                        <td className={styles.td}>
+                            <div className={styles.icons}>
+                                <a className={styles.icon} href="/addevents">
+                                    <Icons.HiPencil />
+                                </a>
+                                <a className={styles.icon} href="">
+                                    <Icons.HiTrash />
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
 }
--- components/EventManager.js
+++ components/EventManager.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Datatable from './Datatable'
+import styles from '../styles/EventManager.module.css'
 
 require('es6-promise').polyfill()
 require('isomorphic-fetch')
@@ -7,7 +8,8 @@ require('isomorphic-fetch')
 const EventManager = () => {
     const [data, setData] = useState([])
     const [query, setQ] = useState('')
-    const [searchColumns, setSearchColumns] = useState(['id', 'name'])
+    const [searchColumns, setSearchColumns] = useState(['description', 'name'])
+    const filter = ['id', 'nation_id', 'cover_img_src']
 
     useEffect(() => {
         fetch('./data.json')
@@ -21,32 +23,51 @@ const EventManager = () => {
         )
     }
     const columns = data[0] && Object.keys(data[0])
+
+    if (typeof columns !== 'undefined') {
+        columns.forEach((word, index) => {
+            if (filter.indexOf(word) !== -1) {
+                columns.splice(index, 2)
+            }
+        })
+    }
+
     return (
         <div>
-            <div>
+            <div className={styles.searchContainer}>
                 <input
+                    className={styles.searchInput}
+                    placeholder="Search"
                     type="text"
                     value={query}
                     onChange={(event) => setQ(event.target.value.toLowerCase())}
                 />
-                {columns &&
-                    columns.map((column) => (
-                        <lable>
-                            <input
-                                type="checkbox"
-                                checked={searchColumns.includes(column)}
-                                onChange={(event) => {
-                                    const checked = searchColumns.includes(column)
-                                    setSearchColumns((prev) =>
-                                        checked
-                                            ? prev.filter((sc) => sc !== column)
-                                            : [...prev, column]
-                                    )
-                                }}
-                            />
-                            {column}
-                        </lable>
-                    ))}
+
+                <ul className={styles.menu}>
+                    <li className={styles.li}>
+                        <a>Filters</a>
+                        <ul>
+                            {columns &&
+                                columns.map((column) => (
+                                    <lable>
+                                        <input
+                                            type="checkbox"
+                                            checked={searchColumns.includes(column)}
+                                            onChange={(event) => {
+                                                const checked = searchColumns.includes(column)
+                                                setSearchColumns((prev) =>
+                                                    checked
+                                                        ? prev.filter((sc) => sc !== column)
+                                                        : [...prev, column]
+                                                )
+                                            }}
+                                        />
+                                        {column}
+                                    </lable>
+                                ))}
+                        </ul>
+                    </li>
+                </ul>
             </div>
             <div>
                 <Datatable data={search(data)} />
