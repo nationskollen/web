3fd2f7f11e393cef77e2df51f836877f55023069
wallskog <carl.wallskog@gmail.com>
2021-05-12 12:03:29 +0200

    changed main and sidebar css and layout

--- components/Card.js
+++ components/Card.js
@@ -2,18 +2,10 @@ import React from 'react'
 import styles from '../styles/Card.module.css'
 import Chart from './Chart'
 
-const Color = [
-    'from-indigo-500 to-blue-500',
-    'from-blue-400 to-blue-300',
-    'from-green-500 to-green-400',
-    'from-yellow-600 to-yellow-500',
-]
-
 const Card = (props) => {
     var color = props.color
-
     return (
-        <div className={styles.container} data-color={styles.color}>
+        <div className={styles.container}>
             <div className={styles.main}>
                 <div></div>
             </div>
