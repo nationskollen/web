1679cf16455915b073a065202821b896e0d2ebb6
Fredrik Engstrand <frewacom@gmail.com>
2021-06-08 11:46:40 +0200

    Added onChange prop to input

--- components/common/Input.tsx
+++ components/common/Input.tsx
@@ -1,4 +1,25 @@
-import React from 'react'
+/**
+ * Renders a generic input. To simplify the props, this uses a more
+ * verbose definition. This also means that you have more freedom when
+ * creating your inputs. There is currently only one style, but you
+ * can modify the styling of each sub-component using `className`.
+ *
+ * Example usage:
+ * ```typescript
+ * <Input.Wrapper label="Email">
+ *     <Input.Icon>
+ *         <MailIcon />
+ *     </Input.Icon>
+ *     <Input.Field type="email" placeholder="din@email.se" required />
+ * </Input.Wrapper>
+ * ```
+ *
+ * The `Input.Icon` component accepts all props that a
+ * regular input element accepts.
+ *
+ * @module Common
+ */
+import React, { useState, useCallback } from 'react'
 
 export type InputSizes = 'small' | 'default' | 'large'
 export type InputStyles = 'transparent'
@@ -15,6 +36,7 @@ export interface FieldProps {
     type: string
     placeholder?: string
     className?: string
+    onChange?: (value: string | null) => void
     [key: string]: unknown
 }
 
@@ -50,11 +72,25 @@ const Wrapper = ({ size, style, label, className, children }: Props) => {
     )
 }
 
-const Field = ({ type, placeholder, className, ...props }: FieldProps) => {
+const Field = ({ type, placeholder, className, onChange, ...props }: FieldProps) => {
     const base = 'flex-1 h-full focus:outline-none bg-transparent text-text-highlight'
     const classes = className ? `${base} ${className}` : base
 
-    return <input type={type} placeholder={placeholder} className={classes} {...props} />
+    const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!onChange) {
+            return
+        }
+
+        const { value } = e.target
+        onChange((!value || value === '') ? null : value)
+    }, [onChange])
+
+    return <input
+        type={type}
+        placeholder={placeholder}
+        className={classes} {...props}
+        onChange={handleChange}
+    />
 }
 
 const Icon = ({ className, children }: IconProps) => {
