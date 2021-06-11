6be72b97e340be983dfe127146e759cfe9aafb4a
Fredrik Engstrand <frewacom@gmail.com>
2021-06-11 21:02:55 +0200

    Remove dialog from dashboard

--- pages/admin/dashboard.tsx
+++ pages/admin/dashboard.tsx
@@ -1,16 +1,9 @@
 import Card from '@common/Card'
 import MainLayout from '@layouts/admin/Main'
-import GeneralDialog from '@common/dialogs/GeneralDialog'
 
 const Dashboard = () => {
     return (
         <Card>
-            <GeneralDialog
-                title="N[got blev fel]"
-                description="Jag 'r en beskrvning'"
-                onCancel={() => console.log('asda')}
-                onConfirm={() => console.log('confirm')}
-            />
             <p>Dashboard</p>
         </Card>
     )
